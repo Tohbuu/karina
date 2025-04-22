95852adb40c4a57b6c840a38057c48d37868165a
Tohbuu <rin96tohsakaaa69@gmail.com>
2025-04-22 18:14:07 +0800

    Remove unused PizzaHub project files and update links in index.html

--- src/pizzahub_full_project/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  if (!req.session.user) return res.redirect('/login');
-  res.render('dashboard', { user: req.session.user });
-});
-
-module.exports = router;
